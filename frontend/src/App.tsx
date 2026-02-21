eeaafd1efa9134e96e06620c4a494bd701483e7d
Joshua Sofela <joshuasofela@gmail.com>
2026-02-21 18:00:53 +0100

    routes defining and dahsboard for professional view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-
+import ProfessionalLayout from "./pages/professional/ProfessionalLayout";
+import ProfessionalDashboard from "./pages/professional/ProfessionalDashboard";
+import ProfessionalPatients from "./pages/professional/ProfessionalPatients";
+import ProfessionalChat from "./pages/professional/ProfessionalChat";
+import ProfessionalReports from "./pages/professional/ProfessionalReports";
+import ProfessionalTemplates from "./pages/professional/ProfessionalTemplates";
+import ProfessionalProfile from "./pages/professional/ProfessionalProfile";
+import ProfessionalSettings from "./pages/professional/ProfessionalSettings";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -16,7 +23,16 @@ const App = () => (
         <Routes>
           <Route path="/" element={<LandingPage />} />
 
-          <Route path="*" element={<NotFound />} />
+          <Route path="/professional" element={<ProfessionalLayout />}>
+            <Route index element={<ProfessionalDashboard />} />
+            <Route path="patients" element={<ProfessionalPatients />} />
+            <Route path="chat/:patientId" element={<ProfessionalChat />} />
+            <Route path="reports" element={<ProfessionalReports />} />
+            <Route path="templates" element={<ProfessionalTemplates />} />
+            <Route path="profile" element={<ProfessionalProfile />} />
+            <Route path="settings" element={<ProfessionalSettings />} />
+          </Route>
+          {/**<Route path="*" element={<NotFound />} />**/}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
